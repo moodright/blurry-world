642a63c63377455ad2b335568dfe340de0a6bb16
moodright <moodright@qq.com>
2021-03-01 14:09:14 +0800

    用户管理模板beta

--- src/main/java/com/moodright/blurryworld/controller/admin/DashBoardController.java
+++ src/main/java/com/moodright/blurryworld/controller/admin/DashBoardController.java
@@ -12,9 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class DashBoardController {
 
+    // 后台管理主页面
     @GetMapping
     public String dashboard() {
         return "admin/dashboard";
+    }
+
+    // 用户管理页面跳转
+    @GetMapping("/user")
+    public String userManagement() {
+        return "admin/user-management";
     }
 
 }
