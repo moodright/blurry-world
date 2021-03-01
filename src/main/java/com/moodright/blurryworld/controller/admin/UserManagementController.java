576e958c96c2122c21a994c31e5a0d05caacb4bf
moodright <moodright@qq.com>
2021-03-01 23:51:11 +0800

    添加自定义Banner

--- src/main/java/com/moodright/blurryworld/controller/admin/UserManagementController.java
+++ src/main/java/com/moodright/blurryworld/controller/admin/UserManagementController.java
@@ -2,14 +2,12 @@ package com.moodright.blurryworld.controller.admin;
 
 import com.moodright.blurryworld.dao.UserDao;
 import com.moodright.blurryworld.pojo.User;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author moodright
